cf3f982e384439a0f6058fc729d603273bb1ebd5
ores97 <36691447+ores97@users.noreply.github.com>
2019-09-03 10:28:20 +0200

    Final commit of code, program full done

--- src/TrainSystem/KiwiLandTrain.java
+++ src/TrainSystem/KiwiLandTrain.java
@@ -8,15 +8,17 @@ public class KiwiLandTrain {
 	ArrayList<String> destinations; //the same as above but with the destinations
 	ArrayList <ArrayList<String>> routesToCalculate; //to input the routes to calculate
 	ArrayList <String> distancesCalculated; //to get the values of the distances calculated
+	String outputResult; //the result of all the calculations in a structure
+	
 	public static void main(String [] args)
 	{
 		//We create the object of the system
-		KiwiLandTrain kiwiLandSystem = new KiwiLandTrain(new ArrayList<KiwiLandRoute>(),new ArrayList<String>(),new ArrayList<String>(),new ArrayList <ArrayList<String>>(),new ArrayList <String>());
+		KiwiLandTrain kiwiLandSystem = new KiwiLandTrain(new ArrayList<KiwiLandRoute>(),new ArrayList<String>(),new ArrayList<String>(),new ArrayList <ArrayList<String>>(),new ArrayList <String>(),"");
 		//We add the routes given by the args
 		kiwiLandSystem.addRoutesGiven(args);
 		if(kiwiLandSystem.getRoutes().size()>0) {
 			//We get all the possible origins and all the possible destinations
-			kiwiLandSystem.addPossibleOrigins(args);
+			kiwiLandSystem.addPossibleOrigins(args); //
 			kiwiLandSystem.addPossibleDestinations(args);
 			//We add the routes that we need to calculate the distance
 			kiwiLandSystem.addRoutesToCalculate();
@@ -28,26 +30,28 @@ public class KiwiLandTrain {
 			}
 		}	
 	}
-	
-	private void printResults() {
+	//method to print the results as the document asked
+	public void printResults() {
 		int i= 0;
-		System.out.print("Graph: ");
+		String outputResult = "Graph: "; //we put the structure of the document for the given routes
+		
 		for(KiwiLandRoute r : routes) {
-			System.out.print(r.toString());
+			outputResult = outputResult + r.toString();
 			i++;
 			if(i != routes.size()) {
-				System.out.print(", ");
+				outputResult = outputResult + ", ";
 			}
 			else {
-				System.out.println("");
+				outputResult = outputResult + "\n";
 			}
 		}
-		System.out.println("Expected Output:");
+		i= 0;
+		outputResult = outputResult + "Expected Output:\n"; //and then we do the same for the distances calculated
 		for(String distance:distancesCalculated) {
 			i++;
-			System.out.println("Output #"+i+": "+distance);
+			outputResult = outputResult + "Output #"+i+": "+distance+"\n";
 		}
-		
+		System.out.print(outputResult);
 	}
 
 	//method to add the routes to calculate in the system
@@ -57,31 +61,18 @@ public class KiwiLandTrain {
 			String routescanned = "";
 			while (!routescanned.equals("-1")) {
 				System.out.println("Enter a route to be calculated: (for example ABC)");
-				System.out.println("If you want to finish adding routes, enter -1");
+				System.out.println("If you want to finish adding routes, enter -1"); //this is to stop asking
 				Scanner scanner = new Scanner(System.in);
-				routescanned = scanner.nextLine();
+				routescanned = scanner.nextLine().toUpperCase(); //the basic system for asking the user, we put the upper case
 				System.out.println("Your route is " + routescanned);
 				if(!routescanned.equals("-1")) { //If the user puts a -1, the program stops asking
 					ArrayList<String> s1 = new ArrayList<String>();
 					for(int i = 0;i<routescanned.length();i++) {
 						s1.add(routescanned.substring(i,i+1));
 					}
-					routesToCalculate.add(s1);
+					routesToCalculate.add(s1); //finally, we add the route to the system
 				}
-				
 			}
-			/*
-			OLD VERSIONS, PREDEFINED BY TEST
-			String[] s1 = {"A","B","C"}; 
-			String[] s2 = {"A","D"};
-			String[] s3 = {"A","D","C"};
-			String[] s4 = {"A","E","B","C","D"};
-			String[] s5 = {"A","E","D"};
-			routesToCalculate.add(s1);
-			routesToCalculate.add(s2);
-			routesToCalculate.add(s3);
-			routesToCalculate.add(s4);
-			routesToCalculate.add(s5); */
 		}catch(Exception e) {
 			System.err.println("Error within the addition of the routes to calculate");
 		}
@@ -114,7 +105,7 @@ public class KiwiLandTrain {
 	public void addPossibleOrigins(String[] args) {
 		for(String argOrigins: args) {
 			try {
-				argOrigins = argOrigins.substring(0, 1);
+				argOrigins = argOrigins.substring(0, 1); //we get only the origin from the substring
 				if(!origins.contains(argOrigins)) {
 					origins.add(argOrigins);
 				}
@@ -128,7 +119,7 @@ public class KiwiLandTrain {
 	public void addPossibleDestinations(String[] args) {
 		for(String argDestinations: args) {
 			try {
-				argDestinations = argDestinations.substring(1, 2);
+				argDestinations = argDestinations.substring(1, 2); //we get only the origin from the substring
 				if(!destinations.contains(argDestinations)) {
 					destinations.add(argDestinations);
 				}
@@ -142,20 +133,26 @@ public class KiwiLandTrain {
 	public void calculateDistances() {
 		try {
 			for(ArrayList<String> stations: routesToCalculate) {
-				int distanceActual = 0;
-				for(int i=0;i<stations.size() - 1;i++) {
-					distanceActual = distanceActual + calculateDistance(stations.get(i),stations.get(i+1));
+				try {
+					int distanceActual = 0;
+					for(int i=0;i<stations.size() - 1;i++) {
+						distanceActual = distanceActual + calculateDistance(stations.get(i),stations.get(i+1)); //we calculate the distance of all edges between stations using the other function
+						if (distanceActual < 0) {//Puting a threshold very low helps getting the exception of No such route
+							distancesCalculated.add("NO SUCH ROUTE");
+							break; //we save time putting this here and breaking the internal loop
+						}
+					}
+					if (distanceActual>=0) { //if everything works, then we put the distance correctly
+						distancesCalculated.add(String.valueOf(distanceActual));
+					}	
+				}catch(Exception e) {
+					System.err.println("Error within the calculation of one route");
+					distancesCalculated.add("ERROR IN CALCULATING DISTANCES");
 				}
-				if (distanceActual<0) { //Puting a threshold very low helps getting the exception of No such route
-					distancesCalculated.add("NO SUCH ROUTE");
-					
-				}else {
-					distancesCalculated.add(String.valueOf(distanceActual));
-				}
+				
 			}
 		}catch(Exception e) {
 			System.err.println("Error within the calculation of the distances in total");
-			distancesCalculated.add("ERROR IN CALCULATING DISTANCES");
 		}
 	}
 	
@@ -168,7 +165,7 @@ public class KiwiLandTrain {
 					distance = r.getDistance();
 				}
 			}
-			return distance;
+			return distance; //If we don't get any possible route, then the distance we return has a value of -10000 and the system knows its a NO ROUTE 
 		}catch(Exception e) {
 			System.err.println("Error within the calculation of the distance between two stations");
 			return -10000;
@@ -184,21 +181,20 @@ public class KiwiLandTrain {
 		this.destinations = new ArrayList<String>();
 		this.routesToCalculate = new ArrayList <ArrayList<String>>();
 		this.distancesCalculated = new ArrayList <String>();
+		this.outputResult = "";
 	}
-	
+	//Constructors with fields
 	public KiwiLandTrain(ArrayList<KiwiLandRoute> routes, ArrayList<String> origins, ArrayList<String> destinations,
-			ArrayList<ArrayList<String>> routesToCalculate, ArrayList<String> distancesCalculated) {
+			ArrayList<ArrayList<String>> routesToCalculate, ArrayList<String> distancesCalculated, String output) {
 		super();
 		this.routes = routes;
 		this.origins = origins;
 		this.destinations = destinations;
 		this.routesToCalculate = routesToCalculate;
 		this.distancesCalculated = distancesCalculated;
+		this.outputResult = output;
 	}
 
-	//Constructors with fields
-	
-	
 	//Getters and Setters part
 	public ArrayList<KiwiLandRoute> getRoutes() {
 		return routes;
@@ -238,6 +234,12 @@ public class KiwiLandTrain {
 
 	public void setDistancesCalculated(ArrayList<String> distancesCalculated) {
 		this.distancesCalculated = distancesCalculated;
+	}
+	public String getOutputResult() {
+		return outputResult;
+	}
+	public void setOutputResult(String outputResult) {
+		this.outputResult = outputResult;
 	}
 	
 	
